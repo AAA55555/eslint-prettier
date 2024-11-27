1b80dc2f791a4b7898ebea31eeb56d7796742958
kondratev <kondratev@elma-bpm.com>
2024-11-27 21:35:28 +0400

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+const eslintConfig = require("./.eslintrc.js");
+const prettierConfig = require("./prettier.config.js");
+const stylelintConfig = require("./stylelint.config.js");
+
+module.exports = {
+  eslintConfig,
+  prettierConfig,
+  stylelintConfig,
+};
